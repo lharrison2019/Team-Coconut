a8b0a86066ada72ec3fc20885c1bc77b9bcfcbbd
Enrique Calle-Matute <enriquecalle-matute@Enriques-MacBook-Pro.local>
2021-11-15 20:32:49 -0500

    added players and pair_count(hand) which looks for pairs in hand removes them and adds a point

--- go_fish.py
+++ go_fish.py
@@ -5,6 +5,11 @@ import random
 
 numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
 suit = ['hearts', 'clubs', 'aces', 'spades']
+player1 = list()
+player1_hand = 0 
+
+player2 = list()
+player2_hand = 0
 
 def player_cards():
     cards = []
@@ -12,3 +17,20 @@ def player_cards():
     random.shuffle(cards)
     return cards
     
+
+    for i in range(5):
+        deck  = cards.pop()
+        player1.append(deck)
+
+    for i in range(5):
+        deck = cards.pop()
+        player2.append(deck)
+
+def pair_count(hand):
+    score = 0 
+    for deck in hand:
+        if hand.count(hand) > 1:
+
+            deck.remove(hand)
+            deck.remove(hand)
+            score += 1 
