73077793062a82b4375f297a195447a03167b2ab
nour662 <nourali1786@gmail.com>
2021-11-15 21:55:24 -0500

    updated with docstrings

--- go_fish.py
+++ go_fish.py
@@ -1,12 +1,11 @@
 import random
 
+
 numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
 suits = ['hearts', 'clubs', 'aces', 'spades']
 deck = []
 
-
 def make_deck():
-
     for num in numbers: 
         for suit in suits: 
               deck.append([num, suit])
@@ -14,11 +13,22 @@ def make_deck():
     random.shuffle(deck)
     return deck
 
-
-def deal_hand(): 
-    hand = []
+def deal(num_of_cards): 
+    """Deals player hands from Deck. 
     
-    for i in range(7):
+    This function will take a specified number of cards from the deck 
+    and returns those cards to a player.  
+
+    Args: 
+        num_of_cards: int representing the amount of cards that 
+        will be taken from the deck and returns to the player. 
+        
+    Returns: 
+        hand: list of cards that will be returned to the player. 
+
+    """
+    hand = []
+    for i in range(num_of_cards):
         card = random.randint(0, len(deck))
         hand.append(deck.pop(card))
     
@@ -34,31 +44,68 @@ def pair_count(hand):
             score += 1 
 
 class Player: 
+    """Parent Class representing a player. 
+    
+    Attributes: 
+        name: string representing name of player 
+        hand: list of cards representing current hand of the player 
+        score: int representing the players score.
+
+    """
     
     def __init__(self, name, hand, score): 
         self.name = name
-        self.hand = deal_hand() 
+        self.hand = deal(7) 
         self.score = 0 
         
     def get_score(self): 
+        """Returns score. 
+        
+        Returns: 
+            self.score: int representing the players score.
+            
+        """
         return self.score
     
     def get_hand(self): 
+        """Returns hand. 
+        
+        Returns: 
+            self.hand: list of cards representing current hand of the player 
+        
+        """
         return self.hand 
     
     
 
 class HumanPlayer(Player): 
+    """Child Class of Player Class representing the human player. 
+    
+    Attributes: 
+        name: string representing name of player 
+        hand: list of cards representing current hand of the player 
+        score: int representing the players score.
+
+    """
     
     super
     
     
 class ComputerPlayer(Player): 
+    """Child Class of Player Class representing the computer player. 
+    
+    Attributes: 
+        name: string representing name of player, with a default of
+            "Computer"
+        hand: list of cards representing current hand of the player 
+        score: int representing the players score.
+
+    """
    
     super
     
     def __init__(self, hand, score , name = "Computer"): 
         self.name = name
-        self.hand = deal_hand() 
+        self.hand = deal(7) 
         self.score = 0 
     
